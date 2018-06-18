1ea29449ae699e225f9e9c85b96cd17838999152
maksim-maslov <brat.maksim@gmail.com>
2018-06-18 14:28:16 +0300

    Добавил структуру файлов проекта

---
+++ app/src/Cook/CookCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular
+  .module('myApp')
+  .controller('CookCtrl', function($scope) {
+
+  });
+
---
+++ app/src/Cook/CookService.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular
+  .module('myApp')
+  .factory('CookService', function ($http) {
+    
+  });
