2e2fa60fcfa9b171aaac1d70d69761706a3be5d5
Maxim <brat.maksim@gmail.com>
2020-12-11 23:58:42 +0300

    Изменил db.js (миграция бд с mLab на atlas)

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : 'mongodb://dbtest:dbtest1@ds263670.mlab.com:63670/drone-cafe'
+    url : 'mongodb://dbtest:dbtest@drone-cafe-shard-00-00.3cc3r.mongodb.net:27017,drone-cafe-shard-00-01.3cc3r.mongodb.net:27017,drone-cafe-shard-00-02.3cc3r.mongodb.net:27017/drone-cafe?ssl=true&replicaSet=atlas-vblitr-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
